cbacfb49d48e92cf21ae8283ce82158742def93c
Md-shahien-khan <shawonkn58@gmail.com>
2025-11-10 00:42:39 +0000

    added primitive data

---
+++ src/primitive.ts
@@ -0,0 +1,35 @@
+// JS -TS 
+// string, number, boolean, undefined, null
+// tytpe script primitive type
+// TS : never, unknown, void
+
+// let userName : string = "Shawon123";
+// userName = 12 Type 'number' is not assignable to type 'string'.
+
+
+// let userId = '123';
+// userId.toFixed() Property 'toFixed' does not exist on type 'string'. Did you mean 'fixed'?
+
+// let userId = 123;
+// userId.toFixed;
+
+
+
+let userName : string = "Shawon123";
+
+
+// Implicit typing
+let isAdmin2 = false 
+// Explicit typing
+let isAdmin : boolean = false;
+
+
+// undefined
+let x;  //isAdmin : boolean = false; 'x' is declared but its value is never read.ts(6133) let x: any
+// if there is no value, that any value so that we can add later like x = 5;
+
+
+//  but if you write 
+let y : undefined = undefined;
+// then you assign something it will show error since you mention undefined already
+// y = 5; Type '5' is not assignable to type 'undefined'.
